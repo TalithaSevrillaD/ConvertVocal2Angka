c5046e411e23e75422a5cd65ccc274e92068bb9e
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-08-14 12:48:42 +0700

    Initial Commit

---
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertvocal2angka;
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+       private static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+        System.out.println("\nConvert kalimat Alay Angka (Vokal ke Angka\n");
+      
+    }
+    public static void main(String[] args){
+    String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
+        tampilJudul(identitas);
+    }
+
+    }
+    
+
